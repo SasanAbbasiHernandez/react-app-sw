785033101748d9a184537c0def7c3bb29b5b1728
Sasan Abbasi Hernandez <sasan.abbasi@tecnologiaypersonas.net>
2024-11-18 15:39:34 +0100

    first

--- src/index.js
+++ src/index.js
@@ -1,23 +1,57 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// Registro manual del Service Worker personalizado
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then((registration) => {
+      console.log('Service Worker registrado con éxito:', registration);
+
+      // Escuchar actualizaciones en el Service Worker
+      registration.addEventListener('updatefound', () => {
+        const newWorker = registration.installing;
+        if (newWorker) {
+          newWorker.addEventListener('statechange', () => {
+            if (newWorker.state === 'installed') {
+              if (navigator.serviceWorker.controller) {
+                // Hay una nueva versión lista
+                showUpdateNotification(registration);
+                console.log("hola")
+                console.log("hola")
+                console.log("hola")
+              }
+            }
+          });
+        }
+      });
+    }).catch((error) => {
+      console.error('Error al registrar el Service Worker:', error);
+    });
+  });
+}
+
+// Mostrar notificación para actualizar
+function showUpdateNotification(registration) {
+  const userConfirmed = window.confirm('¡Nueva versión disponible! ¿Deseas actualizar?');
+  if (userConfirmed) {
+    if (registration.waiting) {
+      // Envía un mensaje para que el Service Worker limpie la caché
+      registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+      registration.waiting.addEventListener('statechange', (event) => {
+        if (event.target.state === 'activated') {
+          console.log('Nueva versión activada, recargando la página...');
+          window.location.reload();
+        }
+      });
+    }
+  }
+}
+
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();

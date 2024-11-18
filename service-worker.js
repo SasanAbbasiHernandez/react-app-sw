f026998e1243dd869a4a89aa6f48ff651ea1fc70
Sasan Abbasi Hernandez <sasan.abbasi@tecnologiaypersonas.net>
2024-11-18 17:06:40 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -2,43 +2,12 @@
 
 const CACHE_NAME = 'app-cache-v9';
 const urlsToCache = [
-  '${process.env.PUBLIC_URL}/', // Raíz de la app
+  'https://sasanabbasihernandez.github.io/react-app-sw/', // Raíz de la app
   'https://sasanabbasihernandez.github.io/react-app-sw/index.html', // Archivo principal
   'https://sasanabbasihernandez.github.io/react-app-sw/static/js/main.js', // Archivos estáticos generados
   'https://sasanabbasihernandez.github.io/react-app-sw/static/css/main.css', // Estilo generado
   // Agrega aquí más rutas según las necesidades de tu app
 ];
-
-// Instalación del Service Worker
-// self.addEventListener('install', (event) => {
-//   console.log('[Service Worker] Instalando...');
-//   event.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       console.log('[Service Worker] Cacheando archivos iniciales');
-//       return cache.addAll(urlsToCache);
-//     })
-//   );
-//   self.skipWaiting(); // Permite la activación inmediata
-// });
-
-// // Activación del Service Worker
-// self.addEventListener('activate', (event) => {
-//   console.log('[Service Worker] Activando...');
-//   event.waitUntil(
-//     caches.keys().then((cacheNames) => {
-//       console.log('[Service Worker] Cachés existentes:', cacheNames);
-//       return Promise.all(
-//         cacheNames.map((cacheName) => {
-//           if (cacheName !== CACHE_NAME) {
-//             console.log('[Service Worker] Eliminando caché antigua:', cacheName);
-//             return caches.delete(cacheName);
-//           }
-//         })
-//       );
-//     })
-//   );
-//   return self.clients.claim(); // Reclama control de los clientes
-// });
 
 self.addEventListener('install', (event) => {
   console.log('[Service Worker] Instalando...');
@@ -107,30 +76,3 @@ self.addEventListener('message', (event) => {
   }
 });
 
-// Manejar eventos de fetch
-// self.addEventListener('fetch', (event) => {
-//   console.log('[Service Worker] Fetch para:', event.request.url);
-//   event.respondWith(
-//     fetch(event.request)
-//       .then((response) => {
-//         // Almacena una copia actualizada en la caché
-//         return caches.open(CACHE_NAME).then((cache) => {
-//           cache.put(event.request, response.clone());
-//           return response;
-//         });
-//       })
-//       .catch(() => {
-//         // Si no hay conexión, intenta servir desde la caché
-//         return caches.match(event.request).then((response) => {
-//           if (response) {
-//             console.log('[Service Worker] Sirviendo desde caché:', event.request.url);
-//             return response;
-//           }
-          
-//           // Si no está en la caché y la red falla, muestra un error
-//           console.log('[Service Worker] Respuesta desde red:', event.request.url);
-//           return Promise.reject('No se encontró en la caché ni se pudo descargar de la red.');
-//         });
-//       })
-//   );
-// });

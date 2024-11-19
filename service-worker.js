5520c2088759be343ce8d3b069e92185d80bd342
Sasan Abbasi Hernandez <sasan.abbasi@tecnologiaypersonas.net>
2024-11-19 09:12:18 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-restricted-globals */
 
-const CACHE_NAME = 'app-cache-v4';
+const CACHE_NAME = 'app-cache-v5';
 const urlsToCache = [
   './', // Raíz de la app
   './index.html', // Archivo principal
